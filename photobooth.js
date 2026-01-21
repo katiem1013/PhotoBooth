eb0df0f3a93b43a608361ae59b2c77abd4bca86a
Katie <74774763+katiem1013@users.noreply.github.com>
2026-01-21 23:11:56 +0000

    Added image capture

--- photobooth.js
+++ photobooth.js
@@ -1,3 +1,8 @@
+var width = 288;
+var height = 192;
+
+var photoStage = 0;
+
 const elements = {
   video: document.getElementById('liveVideo'),
   canvas: document.getElementById('canvas'),
@@ -6,11 +11,38 @@ const elements = {
 
 
 const cameraSetup= () => {
-  navigator.mediaDevices.getUserMedia({ video: { width: 288, height: 192, facingMode: 'user' }, audio: false })
+  navigator.mediaDevices.getUserMedia({ video: { width: width, height: height, facingMode: 'user' }, audio: false })
     .then(stream => { 
         elements.video.srcObject = stream; 
         elements.video.play(); })
     .catch(err => alert('Camera access failed: ' + err));
 };
+
+function takePhoto(){ 
+    if(photoStage == 0){
+        photoStage = +   1;
+        const context = canvas.getContext("2d");
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(elements.video, 0, 0, width, height);
+
+            const data = canvas.toDataURL("image/png");
+            elements.video.setAttribute("src", data);
+        } else {
+            clearPhoto();
+        }
+    }
+}
+
+function clearPhoto() {
+  const context = canvas.getContext("2d");
+  context.fillStyle = "#aaaaaa";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  const data = canvas.toDataURL("image/png");
+  elements.video.setAttribute("src", data);
+}
+
 
 cameraSetup();
