5df3f2681c40eb333fba58ca34f24717639fc505
Katie <74774763+katiem1013@users.noreply.github.com>
2026-01-22 16:48:02 +0000

    restarted - camera is back to working

--- photobooth.js
+++ photobooth.js
@@ -1,67 +1,56 @@
-var width = 1176;
-var height = 1470;
+let width = 288;
+let height = 192;
+let streaming = false;
 
 var photoStage = 0;
 
 const QUATER = height/4;
 
 const elements = {
-  video: document.getElementById('liveVideo'),
+  video: document.getElementById('camera-stream'),
+  shutterButton: document.getElementById('shutter'),
   canvas: document.getElementById('photoCanvas'),
   ctx: document.getElementById('photoCanvas').getContext('2d')
 };
 
-// move video to quater
-const moveVideoToQuater = i => {
+elements.video.addEventListener(
+  "canplay",
+  (ev) => {
+    if(!streaming){
+      height= elements.video.videoHeight / (elements.video.videoWidth / width)
+
+      elements.canvas.setAttribute("width",width);
+      elements.canvas.setAttribute("height",height);
+      elements.video.setAttribute("width",width);
+      elements.video.setAttribute("height",height);
+      streaming = true;
+    }
+  },
+  false
+);
+
+const cameraSetup = () => {
   const { video } = elements;
-  video.style.display = 'block';
-  video.style.top = i === 0 ? '0' : '25%';
-  video.style.left = '0';
-  video.style.width = '100%';
-  video.style.height = '25%';
+
+  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia({video:true})){
+    navigator.mediaDevices
+      .getUserMedia({video:true})
+      .then((stream)=>{
+        video.srcObject = stream;
+        video.play();
+      })
+  }
 };
 
-
-const cameraSetup= () => {
-  navigator.mediaDevices.getUserMedia({ video: { width: { ideal: 2560 }, height: { ideal: 1440 }, facingMode: 'user' }, audio: false })
-    .then(stream => { 
-        elements.video.srcObject = stream; 
-        elements.video.play(); 
-        moveVideoToQuater(0);})
-    .catch(err => alert('Camera Error: ' + err));
+const takePhoto = () => { 
+  const { video, canvas, ctx} = elements;
+  canvas.width = width;
+  canvas.height = height;
+  
+  ctx.save();
+  ctx.drawImage(video, 0, 0, width, height);
 };
 
-function takePhoto(){ 
-    
-    const context = photoCanvas.getContext("2d");
-    const yOffset = photoStage === 0 ? 0 : height/4;
-
-    const targetAspect = width/(height/2), vAspect = elements.video.videoWidth/ elements.video.videoHeight;
-    let sx, sy, swidth, sheight;
-
-    if (vAspect > targetAspect) { sheight = elements.video.videoHeight; swidth = elements.video.videoHeight * targetAspect; sx = (elements.video.videoWidth- swidth) / 4; sy = 0; }
-    else { swidth = elements.video.videoWidth; sheight = elements.video.videoWidth / targetAspect; sx = 0; sy = (elements.video.videoHeight - sheight) / 4; }
-
-
-    context.drawImage(elements.video, 0, 0, width, QUATER);
-
-    elements.ctx.save();
-    elements.ctx.translate(width, 0);
-    elements.ctx.scale(-1, 1);
-    elements.ctx.drawImage(elements.video, sx, sy, swidth, sheight, 0, yOffset, width, QUATER);
-    elements.ctx.restore();
-
-
-    photoStage ++;
-    
-    if(photoStage === 1){moveVideoToQuater(2);}
-    else if(photoStage === 2){moveVideoToQuater(3);}
-    else if(photoStage === 3){moveVideoToQuater(4);}
-    else if (photoStage === 5){console.log("printing photos")}
-
-
-    console.log(photoStage);
-}
-
+elements.shutterButton.addEventListener('click', () => takePhoto());
 
 cameraSetup();
