ebdf95a6c58a401fb729e6439c4a929aa8a1ed77
Katie <74774763+katiem1013@users.noreply.github.com>
2026-03-25 22:35:20 +0000

    filters are now working - had to rework some code

--- javascript/photobooth.js
+++ javascript/photobooth.js
@@ -4,7 +4,7 @@ let streaming = false;
 
 var photoStage = 0;
 var frames=1;
-var filter=1;
+var currentFilter=1;
 
 
 const elements = {
@@ -26,41 +26,39 @@ elements.nextFilter.addEventListener("click", nextfilter);
 elements.prevFilter.addEventListener("click", prevfilter);
 
 function nextfilter(){
-  filter++;
-  if(filter >= 6)
+  currentFilter++;
+  if(currentFilter >= 6)
   {
-    filter=1;
+    currentFilter=1;
   }
-  
-  filterUpdate();
 }
 
 function prevfilter(){
-  filter--;
-  if(filter <= 0)
+  currentFilter--;
+  if(currentFilter <= 0)
   {
-    filter=5;
+    currentFilter=5;
   }
-  
-  filterUpdate();
 }
 
-const filterUpdate = () => {
-  const { canvas, ctx} = elements;
 
-  if (filter === 1) {
-    canvas.style.filter = `${"none"} translateZ(0)`;
-    ctx.filter = "none";
-} 
-else if (filter === 2) {
-    canvas.style.filter = `${"grayscale(100%)"} translateZ(0)`;
-    canvas.style.webkitFilter = `${"grayscale(100%)"} translateZ(0)`;
-    ctx.filter = "grayscale(100%)";
-}
+const draw = () => {
+  const { video, canvas, ctx } = elements;
+ let filterString = "none";
 
-  
-  console.log(filter);
-  
+  if (currentFilter === 1) filterString = "none";
+  else if (currentFilter === 2) filterString = "grayscale(100%)";
+  else if (currentFilter === 3) filterString = "sepia(100%)";
+  else if (currentFilter === 4) filterString = "invert(100%)";
+  else if (currentFilter === 5) filterString = "brightness(1.5) saturate(1.5)";
+
+  canvas.style.filter = filterString;
+
+  console.log(currentFilter);
+
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(draw);
+
 }
 
 
@@ -87,7 +85,7 @@ function prevBTTN(){
 }
 
 const frameUpdate = () => {
-  document.getElementById('frame').src="images/frame"+frames+".png";
+  document.getElementById('frame').src="../images/frame"+frames+".png";
   console.log(frames);
 }
 
@@ -145,25 +143,54 @@ const takePhoto = () => {
   if(photoStage < 4){
     photoStage++;
     const { video, ctx, canvas} = elements;
+
     canvas.width = width;
     canvas.height = height;
 
-    ctx.drawImage(video,0, 0, width, height);
+    ctx.drawImage(video, 0, 0, width, height);
+
+    if (currentFilter > 1) {
+      const imgData = ctx.getImageData(0, 0, width, height);
+      const data = imgData.data;
+
+      for (let i = 0; i < data.length; i += 4) {
+        let r = data[i], g = data[i + 1], b = data[i + 2];
+
+        if (currentFilter === 2) { // Grayscale
+          const avg = (r + g + b) / 3;
+          data[i] = data[i + 1] = data[i + 2] = avg;
+        } 
+        else if (currentFilter === 3) { // Sepia formula
+          data[i]     = (r * 0.393) + (g * 0.769) + (b * 0.189);
+          data[i + 1] = (r * 0.349) + (g * 0.686) + (b * 0.168);
+          data[i + 2] = (r * 0.272) + (g * 0.534) + (b * 0.131);
+        } 
+        else if (currentFilter === 4) { // Invert formula
+          data[i]     = 255 - r;
+          data[i + 1] = 255 - g;
+          data[i + 2] = 255 - b;
+        }
+        else if (currentFilter === 5) { // Brighten (approx 1.5x)
+          data[i]     = Math.min(255, r * 1.5);
+          data[i + 1] = Math.min(255, g * 1.5);
+          data[i + 2] = Math.min(255, b * 1.5);
+        }
+      }
+      ctx.putImageData(imgData, 0, 0);
+    }
 
     const image = canvas.toDataURL('image.png');
     document.getElementById('img-'+photoStage).src=image;
-
     console.log('snap');
-    
     countdown(3);
   }
 };
 
 function clearPhotos(){
-  document.getElementById('img-1').src='images/background.PNG';
-  document.getElementById('img-2').src='images/background.PNG';
-  document.getElementById('img-3').src='images/background.PNG';
-  document.getElementById('img-4').src='images/background.PNG';
+  document.getElementById('img-1').src='../images/background.PNG';
+  document.getElementById('img-2').src='../images/background.PNG';
+  document.getElementById('img-3').src='../images/background.PNG';
+  document.getElementById('img-4').src='../images/background.PNG';
 
   photoStage=0;
 
@@ -202,3 +229,4 @@ document.getElementById('download-bttn').onclick = function(){
 elements.shutterButton.addEventListener('click', () => countdown(3));
 
 cameraSetup();
+draw();
